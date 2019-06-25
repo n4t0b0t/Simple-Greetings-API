cc984c184cdda931bde2b98e8d02fe325244bf24
n4t0b0t <natalieong1990@users.noreply.github.com>
2019-06-25 11:19:57 +0800

    adds basic hello world greeting

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send(`Hello, i got a ${req.method} request`);
+});
+
+app.put("/", (req, res) => {
+  res.send(`Hello, i got a ${req.method} request`);
+});
+
+app.delete("/", (req, res) => {
+  res.send(`Hello, i got a ${req.method} request`);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`You're listening to the smooth sounds of port ${PORT}...`);
+});
